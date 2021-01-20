9bbeca7142b6fde6669e0dea08a28ec3ae3b4a4b
Dhanya Aleena K (iDEAS-Cloud Transformation - iDEAS-Cloud Transformation) <dh20184617@wipro.com>
2021-01-20 11:51:14 +0530

    first commit

---
+++ src/test/java/io/training/BootwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
